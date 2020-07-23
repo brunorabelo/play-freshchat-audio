3b14f7b7365d9b8874f137fb0803cc28dd479e69
Denis Campos <denis.campos@buser.com.br>
2020-07-22 22:45:07 -0300

    Evita ficar baixando arquivos a cada clique e permite pausar o áudio

--- script.js
+++ script.js
@@ -1,51 +1,119 @@
 (function () {
-  async function playAudio (url, playButton) {
-    await new Audio(url).play()
-    playButton.classList.replace('fa-spinner', 'fa-play')
-    playButton.classList.remove('fa-spin')
-    document.getElementById("downloadFile").remove()
-    loadButtons()
+  let audio = null
+  let button = null
+  let urls = {}
+
+  async function playAudio (url) {
+    audio = new Audio(url)
+    await audio.play()
+    addPauseButton()
+    onEndAudio()
+
+    file = document.getElementById('downloadFile')
+    if (file) file.remove()
   };
 
-  function loop (playButton, counter = 0) {
-    file = document.getElementById("downloadFile")
+  function loop (counter = 0) {
+    file = document.getElementById('downloadFile')
     if (counter >= 10) {
       loadButtons()
+      button.classList.remove('fa-spin')
+      button.classList.replace('fa-spinner', 'fa-play')
       return
     }
 
     if (!file || !file.src) {
+      console.log('counter ' + counter)
       setTimeout(() => {
-        loop(playButton, counter)
+        loop(counter)
       }, 500);
       counter++
+      console.log('----------')
       return
     }
 
-    playAudio(file.src, playButton)
+    btnClass = getButtonClass(button)
+    urls[btnClass] = file.src
+    playAudio(file.src)
   }
 
   function disableAllPlayers() {
-    document.querySelectorAll(".download-file>a").forEach(e => {
-      e.firstElementChild.style.color = "gray"
-      e.removeEventListener("click", addPlayButton)
+    document.querySelectorAll('.download-file>a').forEach(e => {
+      e.firstElementChild.style.color = 'gray'
+      button.style.color = '#0053bf'
+      e.removeEventListener('click', loadFile)
+      e.addEventListener('click', blockDownload)
     })
   }
 
-  function addPlayButton(event) {
-    e = event.target
-    e.classList.replace('fa-play', 'fa-spinner')
-    e.classList.add('fa-spin')
-
-    disableAllPlayers()
-    loop(e)
+  // bloquei o download dos arquivos enquando algum áudio está rolando
+  function blockDownload(event) {
+    event.stopPropagation()
   }
 
+  function loadFile (event) {
+    if (button && audio) {
+      event.stopPropagation()
+      return
+    }
+
+    button = event.target
+    btnClass = getButtonClass(button)
+    button.classList.replace('fa-play', 'fa-spinner')
+    button.classList.add('fa-spin')
+    disableAllPlayers()
+
+    if (urls[btnClass]) {
+      event.stopPropagation()
+      playAudio(urls[btnClass])
+      return
+    }
+
+    loop()
+  }
+
+  function addPauseButton () {
+    button.classList.remove('fa-spin')
+    button.classList.replace('fa-spinner', 'fa-pause')
+    button.addEventListener('click', pauseAudio)
+  }
+
+  function pauseAudio (event) {
+    if (!audio) return
+    event.stopPropagation()
+    audio.pause()
+    button.removeEventListener('click', pauseAudio)
+    button.classList.replace('fa-pause', 'fa-play')
+    button.classList.remove('fa-spin')
+    button = null
+    audio = null
+    loadButtons()
+  }
+
+  function onEndAudio() {
+    audio.onended = () => {
+      button.classList.replace('fa-pause', 'fa-play')
+      button = null
+      audio = null
+      loadButtons()
+    }
+  }
+
+  function getButtonClass(button) {
+    const buttonClasses = []
+    button.classList.forEach(c => buttonClasses.push(c))
+    btnClass = buttonClasses.find(c => /pfa-btn-[\d]/.test(c))
+    return btnClass
+  }
+
+  // Busca áudios na conversa e adiciona botão de 'play'
   function loadButtons() {
-    document.querySelectorAll(".download-file>a").forEach(e => {
+    document.querySelectorAll('.download-file>a').forEach((e, i) => {
+      e.firstElementChild.classList.add(`pfa-btn-${i}`)
       e.firstElementChild.classList.replace('fa-arrow-circle-o-down', 'fa-play')
-      e.firstElementChild.style.color = "#0053bf"
-      e.addEventListener("click", addPlayButton)
+      e.firstElementChild.style.color = '#0053bf'
+      e.addEventListener('click', loadFile)
+      e.removeEventListener('click', blockDownload)
     });
   }
 
